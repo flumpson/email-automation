6c2414c68f11ca777e200c083d5f55e3e06b8814
Ryan Brandt <ryan@Ryans-MacBook-Pro-3.bos.cambridgesemantics.com>
2017-08-16 16:37:04 -0400

    succesful test

--- execute.py
+++ execute.py
@@ -2,6 +2,6 @@ from pybin import mailing
 
 if __name__ == '__main__':
     email_info = mailing.loadData(
-        "./csv/creds.csv", "./csv/group1.csv", "Testing 123", "", "./message/intro.txt")
+        "./csv/creds.csv", "./csv/group1.csv", "Testing 123", "smtp.gmail.com", "./message/intro.txt")
     mailing.sendIntroMessage(email_info[0], email_info[1], email_info[
                      2], email_info[3], email_info[4], email_info[5])
