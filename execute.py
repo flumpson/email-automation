d25e70583cbae57319dac271d71ebbb5cfbcd570
Ryan Brandt <ryan@Ryans-MacBook-Pro-3.bos.cambridgesemantics.com>
2017-08-21 14:15:31 -0400

    adds settings file

--- execute.py
+++ execute.py
@@ -1,7 +1,11 @@
 from pybin import mailing
+import json
 
 if __name__ == '__main__':
-    email_info = mailing.loadData(
-        "./csv/creds.csv", "./csv/group1.csv", "Testing 123", "smtp.gmail.com", "./message/intro.txt")
+    fp = open("settings.json", "rw")
+    settings = json.load(fp)
+    print settings["credentials_path"]
+    email_info = mailing.loadData(settings["credentials_path"], settings["email_metadata_path"],  settings["subject"], settings[
+                                  "smtp_server"], settings["message_path"], settings["to_replace_csv_path"], settings["replace_with_csv_path"])
     mailing.sendIntroMessage(email_info[0], email_info[1], email_info[
-                     2], email_info[3], email_info[4], email_info[5])
+                             2], email_info[3], email_info[4], email_info[5])
--- pybin/mailing.py
+++ pybin/mailing.py
@@ -6,45 +6,46 @@ from email.MIMEText import MIMEText
 
 
 # modular data loading from dir files
-def loadData(credCSV, groupCSV, subject, smtp, messageFileName):
+def loadData(credCSV, groupCSV, subject, smtp, messageFileName, patternCSV, replaceCSV):
     credgroupDataObj = d.Data(credCSV)
     credDataRow = credgroupDataObj.get_row(0)
     fromEmail = credDataRow[0]
     password = credDataRow[1]
     smtp = smtp
     groupDataObj = d.Data(groupCSV)
-    return (fromEmail, password, subject, smtp, groupDataObj, messageFileName)
+    patternDataObj = d.Data(patternCSV)
+    replaceDataObj = d.Data(replaceCSV)
+    return (fromEmail, password, subject, smtp, groupDataObj, messageFileName, patternDataObj, replaceDataObj)
 
 
 # replaces generic terms in txt to form custom, targeted messages
-def customMessage(patternArr, replaceArr, messageFileName):
-    fp = open(messageFileName, 'rU')
-    message = fp.read()
+def customMessage(patternArr, replaceArr, message):
     for x in range(len(patternArr)):
         message = message.replace(patternArr[x], replaceArr[x])
     return message
 
 # sends the messages, crafting each one to target the individual
 # NOTE the groupDataObj has 3 columns [PERSON_NAME, EMAIL, COMPANY/INSTITUTION]
-
-
-def sendIntroMessage(fromEmail, password, subject, smtp, groupDataObj, messageFileName):
+def sendIntroMessage(fromEmail, password, subject, smtp, groupDataObj, messageFileName, patternDataObj, replaceDataObj):
     server = smtplib.SMTP(smtp)
     server.starttls()
     server.login(fromEmail, password)
+    fp = open(messageFileName, 'rU')
+    message = fp.read()
     for x in range(groupDataObj.get_num_rows()):
         # grab a row
-        data = groupDataObj.get_row(x)
+        groupData = groupDataObj.get_row(x)
         # only take first name, lose the last
-        temp = data[0].split(" ")
-        data[0] = temp[0]
+        temp = groupData[0].split(" ")
+        groupData[0] = temp[0]
         # set the addresses and headers
         msg = MIMEMultipart()
         msg['From'] = fromEmail
-        msg['To'] = data[1]
-        msg['Subject'] = data[2] + " " + subject
-        body = customMessage(data[0], data[2], messageFileName)
+        msg['To'] = groupData[1]
+        msg['Subject'] = groupData[2] + " " + subject
+        body = customMessage(patternDataObj.get_row(
+            0), replaceDataObj.get_row(x), message)
         msg.attach(MIMEText(body, 'plain'))
         print msg.as_string()
-        server.sendmail(fromEmail, data[1], msg.as_string())
+        server.sendmail(fromEmail, groupData[1], msg.as_string())
     server.quit()
